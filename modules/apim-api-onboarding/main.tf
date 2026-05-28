46741088ec36409f40d91651a3d351a569321eee
rambhuvanparmarsingh-dev <rambhuvanparmarsingh@gmail.com>
2026-05-28 21:48:30 +0530

    [KAN-2] Create 'PetStore-Premium' Product and Grant Access to '/pets' Endpoint
    Autonomous execution plan modifications.

--- modules/apim-api-onboarding/main.tf
+++ modules/apim-api-onboarding/main.tf
@@ -51,3 +51,37 @@ resource "azurerm_api_management_product_policy" "product_policy" {
   # हम मानकर चल रहे हैं कि प्रोडक्ट पॉलिसी की फाइल का पाथ भी हम पास करेंगे
   xml_content = file(var.product_policy_xml_path)
 }
+
+resource "azurerm_api_management_product" "petstore_api_PetStore_Premium_product" {
+  product_id            = "PetStore-Premium"
+  api_management_name   = var.apim_name
+  resource_group_name   = var.resource_group_name
+  display_name          = "PetStore-Premium"
+  subscription_required = true
+  approval_required     = false
+  published             = true
+}
+
+resource "azurerm_api_management_product_api" "petstore_api_PetStore_Premium_product_api_link" {
+  api_name            = azurerm_api_management_api.api.name
+  product_id          = azurerm_api_management_product.petstore_api_PetStore_Premium_product.product_id
+  api_management_name = var.apim_name
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_api_management_product_policy" "petstore_api_PetStore_Premium_policy" {
+  product_id          = azurerm_api_management_product.petstore_api_PetStore_Premium_product.product_id
+  api_management_name = var.apim_name
+  resource_group_name = var.resource_group_name
+  xml_content         = file(var.product_PetStore_Premium_policy_xml_path)
+}
+
+resource "azurerm_api_management_subscription" "petstore_api_PetStore_Premium_subscription" {
+  name                = "PetStore-Premium-Subscription"
+  api_management_name = var.apim_name
+  resource_group_name = var.resource_group_name
+  product_id          = azurerm_api_management_product.petstore_api_PetStore_Premium_product.product_id
+  display_name        = "PetStore Premium Subscription"
+  primary_key         = null
+  secondary_key       = null
+}
