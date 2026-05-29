24b4036dc06af3248b78afbd5a94678778fa6260
rambhuvanparmarsingh-dev <rambhuvanparmarsingh@gmail.com>
2026-05-29 22:34:47 +0530

    [KAN-3] Onboard "User Profile Service" API into Azure APIM
    Autonomous execution plan modifications.

---
+++ apis/user-profile-service-api/api.tf
@@ -0,0 +1,9 @@
+module "user-profile-service-api" {
+  source             = "../../modules/apim-api"
+  api_name           = var.api_name
+  display_name       = var.api_display_name
+  path               = var.api_path
+  protocols          = ["https"]
+  swagger_file_path  = "swagger.json"
+  policy_file_path   = "policy.xml"
+}
