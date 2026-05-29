2d398c23fc873446575837b0dc63a7bc878130f9
rambhuvanparmarsingh-dev <rambhuvanparmarsingh@gmail.com>
2026-05-29 23:20:09 +0530

    [KAN-4] Onboard "Product Inventory Service" API into Azure APIM & Apply Rate-Limit Policy
    Autonomous execution plan modifications.

---
+++ apis/product-inventory-service-api/variables.tf
@@ -0,0 +1,39 @@
+variable "api_name" {
+  type        = string
+  description = "The APIM API resource name"
+}
+
+variable "api_display_name" {
+  type        = string
+  description = "Human-readable display name"
+}
+
+variable "api_path" {
+  type        = string
+  description = "The API URL path suffix"
+}
+
+variable "apim_name" {
+  type        = string
+  description = "The APIM instance name"
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "The Azure resource group name"
+}
+
+variable "openapi_spec_path" {
+  type        = string
+  description = "Path to the OpenAPI spec JSON file"
+}
+
+variable "policy_xml_path" {
+  type        = string
+  description = "Path to the boilerplate policy.xml file"
+}
+
+variable "product_policy_xml_path" {
+  type        = string
+  description = "Path to the product-level policy XML file"
+}
